aaa9bec39fc73e74a3cb94e9d3fde0e2cdfbea6c
Lilith <lilithcode32@gmail.com>
2021-05-05 21:32:27 -0500

    Honk

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "shards-ui/dist/css/shards.min.css";
 import '../styles/global.css';
+
 
 const queryClient = new QueryClient()
 
---
+++ pages/creator-detail.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react';
+import Layout from '../components/Layout';
+
+const CreatorDetail = () => {
+
+    return(
+        <Layout title={"Creator Detail"}>
+            Beep
+        </Layout>
+
+
+    )
+}
+
+export default CreatorDetail;
